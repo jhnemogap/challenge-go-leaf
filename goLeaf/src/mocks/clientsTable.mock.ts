31295a2c5fe21e3367855a22f74a5d29d4382c10
jorge.nemoga-MacPro14M1 <jorge.nemoga@rappi.com>
2023-07-13 04:50:01 -0500

    feat(api): mocks as Map()

---
+++ goLeaf/src/mocks/clientsTable.mock.ts
@@ -0,0 +1,26 @@
+export const CLIENTS_MOCK = new Map([
+  [
+    "ABC90001XYZ",
+    {
+      name: "CLARA",
+    },
+  ],
+  [
+    "ABC90002XYZ",
+    {
+      name: "JEEVES",
+    },
+  ],
+  [
+    "ABC90003XYZ",
+    {
+      name: "IRIS",
+    },
+  ],
+  [
+    "ABC90004XYZ",
+    {
+      name: "Fintech #4 (no es cliente)",
+    }
+  ],
+]);
--- goLeaf/src/routes/api/resources/file-upload-url/index.ts
+++ goLeaf/src/routes/api/resources/file-upload-url/index.ts
@@ -1,41 +1,57 @@
 import { BASE_URL } from '../../../../constants/configs.const.ts';
 
+import { API_KEYS_MOCK, CLIENTS_MOCK } from '../../../../mocks/index.ts';
+
 import type { Handlers } from '$fresh/server.ts';
 
 export const handler: Handlers<FileUploadUrlResponse | null> = {
   GET(_req: Request) {
     const apiKey = validateApiKey(_req);
-    if (apiKey === null) {
+    if (apiKey === '') {
       return new Response(
         JSON.stringify({ message: 'You need a credential' }),
         { status: 401, statusText: 'Unauthorized' }
       );
     }
+    const client = getClientByApiKey(apiKey);
+    if (client === null) {
+      return new Response(
+        JSON.stringify({ message: 'Who are you?' }),
+        { status: 403, statusText: 'Forbidden' }
+      );
+    }
     const userId = validateUserId(_req);
-    if (userId === null) {
+    if (userId === '') {
       return new Response(
         JSON.stringify({ message: 'User ID not found' }),
         { status: 404, statusText: 'Not Found' }
       );
     }
-    const body = generateRespBody({ userId, apiKey });
+    const body = generateRespBody({ userId, clientId: client.id });
     return new Response(JSON.stringify(body));
   },
 };
 
-function validateUserId(req: Request): string | null {
+function validateUserId(req: Request): UserId {
   const url = new URL(req.url);
-  return url.searchParams.get('user_id');
+  return url.searchParams.get('user_id') ?? '';
 }
 
-function validateApiKey(req: Request): string | null {
-  return req.headers.get('x-api-key');
+function validateApiKey(req: Request): ApiKey {
+  const apiKey = req.headers.get('x-api-key') ?? '';
+  return API_KEYS_MOCK.has(apiKey) ? apiKey : '';
+}
+
+function getClientByApiKey(apiKey: ApiKey) {
+  const clientId = API_KEYS_MOCK.get(apiKey)?.clienteId ?? '';
+  const client = CLIENTS_MOCK.get(clientId) ?? null;
+  return client ? { ...client, id: clientId } : null;
 }
 
 function generateRespBody(params: GenerateRespBodyParams) {
-  const { apiKey, userId} = params;
+  const { clientId, userId} = params;
   const url = new URL('/', BASE_URL);
-  url.searchParams.set('api-key', apiKey);
+  url.searchParams.set('client_id', clientId);
   url.searchParams.set('user_id', userId);
   url.searchParams.set('jwt', 'JWTfakeJWTfake');
   return { url: url.toString() };
@@ -45,7 +61,11 @@ interface FileUploadUrlResponse {
   url: string;
 }
 
+type ApiKey = string;
+type ClientId = string;
+type UserId = string;
+
 interface GenerateRespBodyParams {
-  apiKey: string;
-  userId: string;
+  clientId: ClientId;
+  userId: UserId;
 }
