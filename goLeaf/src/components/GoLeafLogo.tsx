7f173d344f67e095ee95d79aebf26f81f1cec728
jorge.nemoga-MacPro14M1 <jorge.nemoga@rappi.com>
2023-07-14 03:28:01 -0500

    fix(web): clean previous files and logic to start web

---
+++ goLeaf/src/components/GoLeafLogo.tsx
@@ -0,0 +1,8 @@
+export function GoLeafLogo() {
+  return (
+    <div class="w-max p-3 flex items-center justify-start	gap-2">
+      <img src="/logo.svg" class="w-12 h-12" alt="GoLeaf Logo" />
+      <p class="text-3xl">GoLeaf</p>
+    </div>
+  );
+}
--- goLeaf/src/routes/index.tsx
+++ goLeaf/src/routes/index.tsx
@@ -1,26 +1,17 @@
 import { Head } from "$fresh/runtime.ts";
-import { useSignal } from "@preact/signals";
 
-import Counter from "../islands/Counter.tsx";
+import { GoLeafLogo } from "../components/GoLeafLogo.tsx";
 
 export default function Home() {
-  const count = useSignal(3);
   return (
     <>
       <Head>
-        <title>Fresh App</title>
+        <title>GoLeaf - Automatiza alta de empresas</title>
       </Head>
       <div class="p-4 mx-auto max-w-screen-md">
-        <img
-          src="/logo.svg"
-          class="w-32 h-32"
-          alt="the fresh logo: a sliced lemon dripping with juice"
-        />
-        <p class="my-6">
-          Welcome to `Esto será épico papus`. Try updating this message in the
-          ./routes/index.tsx file, and refresh.
-        </p>
-        <Counter count={count} />
+        <header>
+          <GoLeafLogo />
+        </header>
       </div>
     </>
   );
