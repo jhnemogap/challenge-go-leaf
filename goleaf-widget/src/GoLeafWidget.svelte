3a3be9dacf525be796da92e22c0e3420eca0f261
jhnemogap-MacPro14M1 <jhnemogap@gmail.com>
2023-07-16 10:14:18 -0500

    feat(widget): only cta btn with css

--- goleaf-widget/src/GoLeafWidget.svelte
+++ goleaf-widget/src/GoLeafWidget.svelte
@@ -1,32 +1,35 @@
 <svelte:options customElement={{ tag: "goleaf-widget", shadow: "open" }} />
 
-<script lang="ts">
-  import Counter from './lib/Counter.svelte';
-</script>
+<!--<script lang="ts">-->
+<!--</script>-->
 
 <main>
-  <h1>Vite + Svelte</h1>
-  <div class="card">
-    <Counter />
-  </div>
-  <p>
-    Check out
-		<a
-			target="_blank"
-			rel="noreferrer"
-			href="https://github.com/sveltejs/kit#readme"
-		>
-			SvelteKit
-		</a>
-		, the official Svelte app framework powered by Vite!
-  </p>
-  <p class="read-the-docs">
-    Click on the Vite and Svelte logos to learn more
-  </p>
+	<button class="cta-btn">
+		Subir datos
+	</button>
+<!--		<a-->
+<!--			target="_blank"-->
+<!--			rel="noreferrer"-->
+<!--			href="https://github.com/sveltejs/kit#readme"-->
+<!--		>-->
+<!--			SvelteKit-->
+<!--		</a>-->
 </main>
 
 <style>
-  .read-the-docs {
-    color: #888;
-  }
+	main:global {
+		--brand-color-primary: #26970D;
+		--brand-color-secondary: #CFFAC7;
+		--font-color-primary: midnightblue;
+	}
+	.cta-btn {
+		display: inline-block;
+		padding: 0.5rem 1rem;
+		font-size: 1.25rem;
+		font-weight: bold;
+		text-decoration: none;
+		border-radius: 0.5rem;
+		color: var(--font-color-primary);
+		background-color: var(--brand-color-secondary);
+	}
 </style>
--- goleaf-widget/vite.config.ts
+++ goleaf-widget/vite.config.ts
@@ -1,12 +1,33 @@
-import { defineConfig } from 'vite'
-import { svelte } from '@sveltejs/vite-plugin-svelte'
+import { defineConfig } from 'vite';
+import { svelte } from '@sveltejs/vite-plugin-svelte';
+import sveltePreprocess from 'svelte-preprocess';
+
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [svelte({
-		compilerOptions: {
-			customElement: true,
-			accessors: true,
+	plugins: [
+		svelte({
+			preprocess: sveltePreprocess({}),
+			exclude: '/\\.component\\.svelte$/',
+			emitCss: false,
+		}),
+		svelte({
+			preprocess: sveltePreprocess(),
+			include: '/\\.component\\.svelte$/',
+			compilerOptions: {
+				customElement: true,
+				accessors: true,
+			},
+			emitCss: false,
+		}),
+	],
+	build: {
+		sourcemap: true,
+		target: 'modules',
+		lib: {
+			entry: 'src/main.js',
+			name: '<<name>>',
+			fileName: 'components',
 		},
-	})],
-})
+	},
+});
