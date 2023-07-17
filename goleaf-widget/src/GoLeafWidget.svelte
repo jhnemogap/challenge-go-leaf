e43ab1a894d94e7d5cfc9a8828150b90bdffd78c
Jorge Humberto Nemogá Pinzón <50347390+jhnemogap@users.noreply.github.com>
2023-07-16 19:26:01 -0500

    feat(#3)(widget): transforms the CTA into a web component
    Closes #3

---
+++ goleaf-widget/src/GoLeafWidget.svelte
@@ -0,0 +1,20 @@
+<svelte:options customElement={{ tag: "goleaf-widget", shadow: "open" }} />
+
+<script>
+	import { StartButton } from './components';
+
+	export let apikey = '';
+	export let userid = '';
+</script>
+
+<main>
+	<StartButton apiKey={apikey} userId={userid} />
+</main>
+
+<style>
+	main {
+		--brand-color-primary: #26970D;
+		--brand-color-secondary: #CFFAC7;
+		--font-color-primary: midnightblue;
+	}
+</style>
